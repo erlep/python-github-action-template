3c56f6b6592591fc7d4ae5f9f3e47ae659b56335
zbpgmy <Pavel.Egerle@onsemi.com>
2023-09-08 16:24:08 +0200

    after history clean-up

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import logging.handlers
 import os
 
 import requests
+import time
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -17,18 +18,25 @@ logger_file_handler.setFormatter(formatter)
 logger.addHandler(logger_file_handler)
 
 try:
-    SOME_SECRET = os.environ["SOME_SECRET"]
+  SOME_SECRET = os.environ["SOME_SECRET"]
 except KeyError:
-    SOME_SECRET = "Token not available!"
-    #logger.info("Token not available!")
-    #raise
+  SOME_SECRET = "Token not available!"
+  #logger.info("Token not available!")
+  # raise
 
 
 if __name__ == "__main__":
-    logger.info(f"Token value: {SOME_SECRET}")
+  # import time - strftime - https://bit.ly/3Edt2np
+  s1 = time.strftime("%Y/%m/%d %H:%M:%S")
+  logger.info(f"Token value: {SOME_SECRET}")
+  # r = requests.get('https://weather.talkpython.fm/api/weather/?city=Berlin&country=DE')
+  r = requests.get('https://weather.talkpython.fm/api/weather?city=Brno&country=CZ')
+  if r.status_code == 200:
+    data = r.json()
+    temperature = data["forecast"]["temp"]
+    s2 = f'Weather in Brno: {temperature}'
+    logger.info(s2)
+    # pridani do README.md
+    with open('README.md', "a+") as f:
+      f.write(s1 + ' '+s2+'\n\n')
 
-    r = requests.get('https://weather.talkpython.fm/api/weather/?city=Berlin&country=DE')
-    if r.status_code == 200:
-        data = r.json()
-        temperature = data["forecast"]["temp"]
-        logger.info(f'Weather in Berlin: {temperature}')
